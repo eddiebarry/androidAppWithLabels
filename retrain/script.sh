4413813662e70ec72967bdda626d0d1cf72ab747
Edgar Monis <edgarmonis@Edgars-MacBook-Air.local>
2019-01-05 22:38:10 +0530

    adding data set & scripts

---
+++ retrain/script.sh
@@ -0,0 +1,14 @@
+pip install --upgrade "tensorflow==1.7.*"
+
+IMAGE_SIZE=224
+ARCHITECTURE="mobilenet_1.00_${IMAGE_SIZE}"
+
+python -m scripts.retrain \
+  --bottleneck_dir=tf_files/bottlenecks \
+  --how_many_training_steps=500 \
+  --model_dir=tf_files/models/ \
+  --summaries_dir=tf_files/training_summaries/"${ARCHITECTURE}" \
+  --output_graph=tf_files/retrained_graph.pb \
+  --output_labels=tf_files/retrained_labels.txt \
+  --architecture="${ARCHITECTURE}" \
+  --image_dir=tf_files/data \
